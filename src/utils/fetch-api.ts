d985fd02b621241478e6863037d692cce45f6c7e
Joao Ferreira <joaovlima07@hotmail.com>
2023-03-26 09:55:09 -0300

    fix: dotenv

--- src/utils/fetch-api.ts
+++ src/utils/fetch-api.ts
@@ -5,7 +5,7 @@ export const baseUrl = 'https://bayut.p.rapidapi.com'
 export const fetchApi = async (url: string)=>{
     const {data} = await axios.get((url),{
         headers: {
-            'X-RapidAPI-Key': 'fe1b962b8amsh149991337b475c9p1994fcjsn92acb7120dfe',
+            'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
           } 
     })
